5c6856021eda0fd9c4853da09a1edb374f963ad1
Scott Keplinger <skeplin17@gmail.com>
2020-01-17 22:21:17 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -19,10 +19,15 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-  return (array[index])
+  return (array[index]);
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
    array.shift()
-    return array
+    return array;
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array.slice(1)
+   return array;
 }
